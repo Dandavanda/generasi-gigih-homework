bd7181e7817708bfd3cf4fb9924e9189a36cf3ba
novanda seimahuira <novanda.s@students.amikom.ac.id>
2021-07-17 23:36:58 +0700

    module 2-3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
-import data from './data';
-import Card from './Component/Card';
-import Form from './Component/Form';
+import data from './Data/albums';
+import Cards from './Component/Card';
+import Container from 'react-bootstrap/esm/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import List from './Component/List';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Create Playlist</h1>
-      <Form />
-      <Card image={data.album.images[0].url} title={data.album.name} artis={data.album.artists[0].name}/>
+      <Container>
+      <Row>
+        <Col xs={12} md={8}>
+        <h2>Playlist</h2>
+          {data.map((d)=>{
+          return(
+            <Cards image={d.album.images[0].url} title={d.album.name} artis={d.album.artists[0].name}/>    
+          )
+        })}
+        </Col>
+        <Col xs={6} md={4}>
+          <h3>Popular</h3>
+          {data
+          .filter(d => d.popularity >= 70)
+          .map((d)=>{
+          return(
+            <List  imgList={d.album.images[2].url} nameList = {d.album.name}/>
+          )
+        })}
+        </Col>
+      </Row>
+      </Container>
     </div>
   );
 }
