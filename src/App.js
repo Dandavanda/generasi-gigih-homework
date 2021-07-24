047b275cc389a1ebcfcf0498b1966f594b68246e
novanda seimahuira <novanda.s@students.amikom.ac.id>
2021-07-24 23:39:50 +0700

    modul3-3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Auth from './Page/auth';
 import Container from 'react-bootstrap/esm/Container';
 import Col from 'react-bootstrap/Col'; import Row from 'react-bootstrap/Row';
 import {useState, useEfect} from 'react' ;
-import  axios from 'axios';
+// import  axios from 'axios';
 
 
 function App() {
-
   [accToken, setAccToken] = useState();
   [accesSrcKey, setAccToken] = useState();
   [accesTrack, setAccTrack] = useState();
@@ -28,13 +27,15 @@ function App() {
 
   return (
     <div className="App">
-      <Auth />
+      
       <Container>
       
       <Search />
       <Row>
         <Col xs={12} md={8}>
         <h2>Playlist</h2>
+        <Auth />
+
           {data.map((d)=>{
           return(
             <Cards image={d.album.images[0].url} title={d.album.name} artis={d.album.artists[0].name}/>    
@@ -51,6 +52,7 @@ function App() {
           )
         })}
         </Col>
+        
       </Row>
       </Container>
     </div>
