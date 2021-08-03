69bb3c6f96554cf5f6f0fc4a868a6ca13f46e508
novanda seimahuira <novanda.s@students.amikom.ac.id>
2021-08-03 22:14:08 +0700

    module 5-1 grid flexbox

--- src/Component/Card/index.js
+++ src/Component/Card/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+
+
 const Cards = (props) => {
     return(
         <div className ="cards">
-        <table>
+          <div className="cardImg">
+          <img src={props.image} alt="Album1" className="img-poster"/>
+          </div>
+          <div className="cardCaps">
+              <p>Track Title : {props.title}</p> 
+                <p id="artis"> Artis : {props.artis}</p> 
+                <button class="btn-select"> Select</button>
+          </div>
+
+        {/* <table>
           <tr>
             <td><img src={props.image} alt="Album1" className="img-poster"/></td>
             <td className="box-desc">
@@ -12,7 +23,7 @@ const Cards = (props) => {
               <button class="btn-select"> Select</button>
             </td>
           </tr>
-        </table>
+        </table> */}
       </div>
     )
 }
